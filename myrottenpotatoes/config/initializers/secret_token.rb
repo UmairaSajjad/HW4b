0943f3efdf059b00322ba1150727af466ddf6d3e
UmairaSajjad <saasbook@saasbook.(none)>
2015-04-12 12:16:13 -0300

    Expecting merge conflicts...

---
+++ myrottenpotatoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = 'f8f77b73d2fc90e1b61f194b49124f80965aa669f3b31f8ccdf68db58e35fb981b18ed113c8423e41f4c3e35f2da8e95847e11d9b1c253a22606296ade09a8c3'
